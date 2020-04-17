33d73af1917a744f4c5d3174ee205524dea7dde7
Shantel Brown <Shantel.Brown2021@gmail.com>
2020-04-17 10:33:48 -0700

    cards and info

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import { Alert } from 'reactstrap';
+import axios from "axios";
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,31 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([])
+  
+
+
+  useEffect(() => {
+    axios.get('https://swapi.py4e.com/api/people/')
+    .then(response => {
+        console.log(response.data)
+        setCharacters(response.data.results)
+    })
+  
+    .catch(error => {
+        console.log(error)
+    })
+  },[])
+  
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map(character =>(
+         <Character character={character} />
+      
+      ))}
+    
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,38 @@
 // Write your Character component here
+import React, {useState, useEffect} from 'react';
+import '../App.css';
+import axios from "axios";
+import { Alert,  Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Container, Spinner} from 'reactstrap';
+
+
+const Character = (props) => {
+
+    return (
+
+    <Container>
+        <div>
+        <Card className="card">
+            <CardBody>
+
+    <CardTitle><h2><strong>{props.character.name}</strong><Spinner type="grow" color="primary" /></h2></CardTitle>
+    <CardSubtitle><h3>{props.character.birth_year}</h3></CardSubtitle>
+    <CardSubtitle><h3>{props.character.gender}</h3></CardSubtitle>
+    <CardSubtitle><h3>{props.character.skin_color}</h3></CardSubtitle>
+    <CardSubtitle><h3>{props.character.eye_color}</h3></CardSubtitle>
+    <CardSubtitle><h3>{props.character.hair_color}</h3></CardSubtitle>
+    <CardSubtitle><h3>{props.character.height}</h3></CardSubtitle>
+    <CardSubtitle><h3>{props.character.mass}</h3></CardSubtitle>
+    <CardSubtitle><h3>{props.character.homeworld}</h3></CardSubtitle>
+    <CardSubtitle><h3>{props.character.species}</h3></CardSubtitle>
+    
+
+
+        </CardBody>
+        </Card>
+    </div>
+    </Container>
+    );
+}
+
+export default Character;
